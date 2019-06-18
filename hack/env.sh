ddfef2397d80de5d1408a501630ac74f75c1ea15
Peng Liu <pliu@redhat.com>
2019-06-18 11:51:32 +0800

    Use ocp 4.2 images

--- hack/env.sh
+++ hack/env.sh
@@ -1,5 +1,5 @@
-SRIOV_CNI_IMAGE=quay.io/openshift/origin-sriov-cni:v4.0.0
-SRIOV_DEVICE_PLUGIN_IMAGE=docker.io/nfvpe/sriov-device-plugin:latest
+SRIOV_CNI_IMAGE=quay.io/openshift/origin-sriov-cni:v4.2.0
+SRIOV_DEVICE_PLUGIN_IMAGE=quay.io/openshift/origin-sriov-network-device-plugin:v4.2.0
 RELEASE_VERSION=0.0.1-snapshot
 NAMESPACE=sriov-network-operator
 OPERATOR_NAME=sriov-network-operator
