0556b71569b0d80c964cbea915f4d857c0152002
Peng Liu <pliu@redhat.com>
2019-07-09 00:09:47 -0400

    Move vfio-pci loading function from intel plugin to generic plugin

--- pkg/plugins/generic/generic_plugin.go
+++ pkg/plugins/generic/generic_plugin.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bytes"
+	"os/exec"
 	"reflect"
+	"strconv"
+	"strings"
 
 	"github.com/golang/glog"
 	sriovnetworkv1 "github.com/openshift/sriov-network-operator/pkg/apis/sriovnetwork/v1"
@@ -13,7 +17,16 @@ type GenericPlugin struct {
 	SpecVersion string
 	DesireState *sriovnetworkv1.SriovNetworkNodeState
 	LastState   *sriovnetworkv1.SriovNetworkNodeState
+	LoadVfioDriver uint
 }
+
+const scriptsPath = "bindata/scripts/enable-kargs.sh"
+
+const (
+	unloaded = iota
+	loading
+	loaded
+)
 
 var Plugin GenericPlugin
 
@@ -22,6 +35,7 @@ func init() {
 	Plugin = GenericPlugin{
 		PluginName:  "generic_plugin",
 		SpecVersion: "1.0",
+		LoadVfioDriver: unloaded,
 	}
 }
 
@@ -30,7 +44,7 @@ func (p *GenericPlugin) Name() string {
 	return p.PluginName
 }
 
-// SpecVersion returns the version of the spec expected by the plugin
+// Spec returns the version of the spec expected by the plugin
 func (p *GenericPlugin) Spec() string {
 	return p.SpecVersion
 }
@@ -44,6 +58,18 @@ func (p *GenericPlugin) OnNodeStateAdd(state *sriovnetworkv1.SriovNetworkNodeSta
 
 	if len(state.Spec.Interfaces) > 0 {
 		needDrain = true
+	}
+	if p.LoadVfioDriver != loaded {
+		if needVfioDriver(state) {
+			p.LoadVfioDriver = loading
+			if needReboot, err = tryEnableIommuInKernelArgs(); err != nil {
+				glog.Errorf("generic-plugin OnNodeStateAdd():fail to enable iommu in kernel args: %v", err)
+				return
+			}
+		}
+		if needReboot {
+			needDrain = true
+		}
 	}
 
 	p.DesireState = state
@@ -78,12 +104,32 @@ func (p *GenericPlugin) OnNodeStateChange(old, new *sriovnetworkv1.SriovNetworkN
 			needDrain = true
 		}
 	}
+	if p.LoadVfioDriver != loaded {
+		if needVfioDriver(new) {
+			p.LoadVfioDriver = loading
+			if needReboot, err = tryEnableIommuInKernelArgs(); err != nil {
+				glog.Errorf("generic-plugin OnNodeStateAdd():fail to enable iommu in kernel args: %v", err)
+				return
+			}
+		}
+		if needReboot {
+			needDrain = true
+		}
+	}
 	return
 }
 
 // Apply config change
 func (p *GenericPlugin) Apply() error {
 	glog.Infof("generic-plugin Apply(): desiredState=%v", p.DesireState.Spec)
+	if p.LoadVfioDriver == loading {
+		if err := utils.LoadKernelModule("vfio_pci"); err != nil {
+			glog.Errorf("generic-plugin Apply(): fail to load vfio_pci kmod: %v", err)
+			return err
+		}
+		p.LoadVfioDriver = loaded
+	}
+
 	if p.LastState != nil {
 		glog.Infof("generic-plugin Apply(): lastStat=%v", p.LastState.Spec)
 		if reflect.DeepEqual(p.LastState.Spec.Interfaces, p.DesireState.Spec.Interfaces) {
@@ -104,4 +150,36 @@ func (p *GenericPlugin) Apply() error {
 	p.LastState = &sriovnetworkv1.SriovNetworkNodeState{}
 	*p.LastState = *p.DesireState
 	return nil
+}
+
+func needVfioDriver(state *sriovnetworkv1.SriovNetworkNodeState) bool {
+	for _, iface := range state.Spec.Interfaces {
+		if iface.DeviceType == "vfio-pci" {
+			return true
+		}
+	}
+	return false
+}
+
+func tryEnableIommuInKernelArgs() (bool, error) {
+	glog.Info("generic-plugin tryEnableIommuInKernelArgs()")
+	args := [2]string{"intel_iommu=on", "iommu=pt"}
+	var stdout, stderr bytes.Buffer
+	cmd := exec.Command("/bin/sh", scriptsPath, args[0], args[1])
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+
+	if err := cmd.Run(); err != nil {
+		glog.Errorf("generic-plugin tryEnableIommuInKernelArgs(): fail to enable iommu %s: %v", args, err)
+		return false, err
+	}
+
+	i, err := strconv.Atoi(strings.TrimSpace(stdout.String()))
+	if err == nil {
+		if i > 0 {
+			glog.Infof("generic-plugin tryEnableIommuInKernelArgs(): need to reboot node")
+			return true, nil
+		}
+	}
+	return false, err
 }
--- pkg/plugins/intel/intel_plugin.go
+++ pkg/plugins/intel/intel_plugin.go
@@ -1,14 +1,8 @@
 package main
 
 import (
-	"bytes"
-	"os/exec"
-	"strconv"
-	"strings"
-
 	"github.com/golang/glog"
 	sriovnetworkv1 "github.com/openshift/sriov-network-operator/pkg/apis/sriovnetwork/v1"
-	"github.com/openshift/sriov-network-operator/pkg/utils"
 )
 
 type IntelPlugin struct {
@@ -16,27 +10,15 @@ type IntelPlugin struct {
 	SpecVersion    string
 	DesireState    *sriovnetworkv1.SriovNetworkNodeState
 	LastState      *sriovnetworkv1.SriovNetworkNodeState
-	LoadVfioDriver uint
 }
 
 var Plugin IntelPlugin
-
-const (
-	scriptsPath = "bindata/scripts/enable-kargs.sh"
-)
-
-const (
-	unloaded = iota
-	loading
-	loaded
-)
 
 // Initialize our plugin and set up initial values
 func init() {
 	Plugin = IntelPlugin{
 		PluginName:  "intel_plugin",
 		SpecVersion: "1.0",
-		LoadVfioDriver: unloaded,
 	}
 }
 
@@ -45,7 +27,7 @@ func (p *IntelPlugin) Name() string {
 	return p.PluginName
 }
 
-// SpecVersion returns the version of the spec expected by the plugin
+// Spec returns the version of the spec expected by the plugin
 func (p *IntelPlugin) Spec() string {
 	return p.SpecVersion
 }
@@ -53,76 +35,17 @@ func (p *IntelPlugin) Spec() string {
 // OnNodeStateAdd Invoked when SriovNetworkNodeState CR is created, return if need dain and/or reboot node
 func (p *IntelPlugin) OnNodeStateAdd(state *sriovnetworkv1.SriovNetworkNodeState) (needDrain bool, needReboot bool, err error) {
 	glog.Info("intel-plugin OnNodeStateAdd()")
-
-	return p.OnNodeStateChange(nil, state)
+	return false, false, nil
 }
 
 // OnNodeStateChange Invoked when SriovNetworkNodeState CR is updated, return if need dain and/or reboot node
 func (p *IntelPlugin) OnNodeStateChange(old, new *sriovnetworkv1.SriovNetworkNodeState) (needDrain bool, needReboot bool, err error) {
 	glog.Info("intel-plugin OnNodeStateChange()")
-	needDrain = false
-	needReboot = false
-	err = nil
-
-	if p.LoadVfioDriver == loaded {
-		return
-	}
-
-	if needVfioDriver(new) {
-		p.LoadVfioDriver = loading
-		if needReboot, err = tryEnableIommuInKernelArgs(); err != nil {
-			glog.Errorf("intel-plugin OnNodeStateAdd():fail to enable iommu in kernel args: %v", err)
-			return
-		}
-	}
-	if needReboot {
-		needDrain = true
-	}
-
-	return
+	return false, false, nil
 }
 
 // Apply config change
 func (p *IntelPlugin) Apply() error {
 	glog.Info("intel-plugin Apply()")
-	if p.LoadVfioDriver == loading {
-		if err := utils.LoadKernelModule("vfio_pci"); err != nil {
-			glog.Errorf("intel-plugin Apply(): fail to load vfio_pci kmod: %v", err)
-			return err
-		}
-		p.LoadVfioDriver = loaded
-	}
 	return nil
-}
-
-func needVfioDriver(state *sriovnetworkv1.SriovNetworkNodeState) bool {
-	for _, iface := range state.Spec.Interfaces {
-		if iface.DeviceType == "vfio-pci" {
-			return true
-		}
-	}
-	return false
-}
-
-func tryEnableIommuInKernelArgs() (bool, error) {
-	glog.Info("intel-plugin tryEnableIommuInKernelArgs()")
-	args := [2]string{"intel_iommu=on", "iommu=pt"}
-	var stdout, stderr bytes.Buffer
-	cmd := exec.Command("/bin/sh", scriptsPath, args[0], args[1])
-	cmd.Stdout = &stdout
-	cmd.Stderr = &stderr
-
-	if err := cmd.Run(); err != nil {
-		glog.Errorf("intel-plugin tryEnableIommuInKernelArgs(): fail to enable iommu %s: %v", args, err)
-		return false, err
-	}
-
-	i, err := strconv.Atoi(strings.TrimSpace(stdout.String()))
-	if err == nil {
-		if i > 0 {
-			glog.Infof("intel-plugin tryEnableIommuInKernelArgs(): need to reboot node")
-			return true, nil
-		}
-	}
-	return false, err
 }
