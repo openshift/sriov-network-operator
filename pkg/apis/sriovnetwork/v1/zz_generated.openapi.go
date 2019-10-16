d394839e630c58470bf67b09f45897ec97922159
Zenghui Shi <zshi@redhat.com>
2019-10-15 21:05:13 -0400

    add SriovOperatorConfig API

--- pkg/apis/sriovnetwork/v1/zz_generated.openapi.go
+++ pkg/apis/sriovnetwork/v1/zz_generated.openapi.go
@@ -20,6 +20,9 @@ func GetOpenAPIDefinitions(ref common.ReferenceCallback) map[string]common.OpenA
 		"github.com/openshift/sriov-network-operator/pkg/apis/sriovnetwork/v1.SriovNetworkNodeStateStatus":  schema_pkg_apis_sriovnetwork_v1_SriovNetworkNodeStateStatus(ref),
 		"github.com/openshift/sriov-network-operator/pkg/apis/sriovnetwork/v1.SriovNetworkSpec":             schema_pkg_apis_sriovnetwork_v1_SriovNetworkSpec(ref),
 		"github.com/openshift/sriov-network-operator/pkg/apis/sriovnetwork/v1.SriovNetworkStatus":           schema_pkg_apis_sriovnetwork_v1_SriovNetworkStatus(ref),
+		"github.com/openshift/sriov-network-operator/pkg/apis/sriovnetwork/v1.SriovOperatorConfig":          schema_pkg_apis_sriovnetwork_v1_SriovOperatorConfig(ref),
+		"github.com/openshift/sriov-network-operator/pkg/apis/sriovnetwork/v1.SriovOperatorConfigSpec":      schema_pkg_apis_sriovnetwork_v1_SriovOperatorConfigSpec(ref),
+		"github.com/openshift/sriov-network-operator/pkg/apis/sriovnetwork/v1.SriovOperatorConfigStatus":    schema_pkg_apis_sriovnetwork_v1_SriovOperatorConfigStatus(ref),
 	}
 }
 
@@ -379,6 +382,90 @@ func schema_pkg_apis_sriovnetwork_v1_SriovNetworkStatus(ref common.ReferenceCall
 			SchemaProps: spec.SchemaProps{
 				Description: "SriovNetworkStatus defines the observed state of SriovNetwork",
 				Type:        []string{"object"},
+			},
+		},
+	}
+}
+
+func schema_pkg_apis_sriovnetwork_v1_SriovOperatorConfig(ref common.ReferenceCallback) common.OpenAPIDefinition {
+	return common.OpenAPIDefinition{
+		Schema: spec.Schema{
+			SchemaProps: spec.SchemaProps{
+				Description: "SriovOperatorConfig is the Schema for the sriovoperatorconfigs API",
+				Type:        []string{"object"},
+				Properties: map[string]spec.Schema{
+					"kind": {
+						SchemaProps: spec.SchemaProps{
+							Description: "Kind is a string value representing the REST resource this object represents. Servers may infer this from the endpoint the client submits requests to. Cannot be updated. In CamelCase. More info: https://git.k8s.io/community/contributors/devel/api-conventions.md#types-kinds",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+					"apiVersion": {
+						SchemaProps: spec.SchemaProps{
+							Description: "APIVersion defines the versioned schema of this representation of an object. Servers should convert recognized schemas to the latest internal value, and may reject unrecognized values. More info: https://git.k8s.io/community/contributors/devel/api-conventions.md#resources",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+					"metadata": {
+						SchemaProps: spec.SchemaProps{
+							Ref: ref("k8s.io/apimachinery/pkg/apis/meta/v1.ObjectMeta"),
+						},
+					},
+					"spec": {
+						SchemaProps: spec.SchemaProps{
+							Ref: ref("github.com/openshift/sriov-network-operator/pkg/apis/sriovnetwork/v1.SriovOperatorConfigSpec"),
+						},
+					},
+					"status": {
+						SchemaProps: spec.SchemaProps{
+							Ref: ref("github.com/openshift/sriov-network-operator/pkg/apis/sriovnetwork/v1.SriovOperatorConfigStatus"),
+						},
+					},
+				},
+			},
+		},
+		Dependencies: []string{
+			"github.com/openshift/sriov-network-operator/pkg/apis/sriovnetwork/v1.SriovOperatorConfigSpec", "github.com/openshift/sriov-network-operator/pkg/apis/sriovnetwork/v1.SriovOperatorConfigStatus", "k8s.io/apimachinery/pkg/apis/meta/v1.ObjectMeta"},
+	}
+}
+
+func schema_pkg_apis_sriovnetwork_v1_SriovOperatorConfigSpec(ref common.ReferenceCallback) common.OpenAPIDefinition {
+	return common.OpenAPIDefinition{
+		Schema: spec.Schema{
+			SchemaProps: spec.SchemaProps{
+				Description: "SriovOperatorConfigSpec defines the desired state of SriovOperatorConfig",
+				Type:        []string{"object"},
+				Properties: map[string]spec.Schema{
+					"enableInjector": {
+						SchemaProps: spec.SchemaProps{
+							Description: "INSERT ADDITIONAL SPEC FIELDS - desired state of cluster Important: Run \"operator-sdk generate k8s\" to regenerate code after modifying this file Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html",
+							Type:        []string{"boolean"},
+							Format:      "",
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func schema_pkg_apis_sriovnetwork_v1_SriovOperatorConfigStatus(ref common.ReferenceCallback) common.OpenAPIDefinition {
+	return common.OpenAPIDefinition{
+		Schema: spec.Schema{
+			SchemaProps: spec.SchemaProps{
+				Description: "SriovOperatorConfigStatus defines the observed state of SriovOperatorConfig",
+				Type:        []string{"object"},
+				Properties: map[string]spec.Schema{
+					"injector": {
+						SchemaProps: spec.SchemaProps{
+							Description: "INSERT ADDITIONAL STATUS FIELD - define observed state of cluster Important: Run \"operator-sdk generate k8s\" to regenerate code after modifying this file Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+				},
 			},
 		},
 	}
