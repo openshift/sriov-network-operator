7a67cee37cbd00639e23a00ee3c02d9014653911
root <root@nfvpe-23.oot.lab.eng.bos.redhat.com>
2019-11-01 16:54:44 -0400

    Fix json field for min/max_tx_rate
    The SRIOV CNI expects min_tx_rate and
    max_tx_rate in the json.
    
    Signed-off-by: vpickard <vpickard@redhat.com>

--- pkg/apis/sriovnetwork/v1/sriovnetwork_types.go
+++ pkg/apis/sriovnetwork/v1/sriovnetwork_types.go
@@ -38,10 +38,10 @@ type SriovNetworkSpec struct {
 	LinkState string `json:"linkState,omitempty"`
 	// +kubebuilder:validation:Minimum=0
 	// Minimum tx rate, in Mbps, for the VF. Defaults to 0 (no rate limiting). min_tx_rate should be <= max_tx_rate.
-	MinTxRate *int `json:"minTxRate,omitempty"`
+	MinTxRate *int `json:"min_tx_rate,omitempty"`
 	// +kubebuilder:validation:Minimum=0
 	// Maximum tx rate, in Mbps, for the VF. Defaults to 0 (no rate limiting)
-	MaxTxRate *int `json:"maxTxRate,omitempty"`
+	MaxTxRate *int `json:"max_tx_rate,omitempty"`
 }
 
 // SriovNetworkStatus defines the observed state of SriovNetwork
