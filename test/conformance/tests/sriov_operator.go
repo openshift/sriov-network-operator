884e6c12d151d84528f39578e5bc32d894a9bea7
mmirecki <mmirecki@redhat.com>
2020-07-13 12:39:08 +0200

    Move the resource injector test to shared policy tests

--- test/conformance/tests/sriov_operator.go
+++ test/conformance/tests/sriov_operator.go
@@ -177,6 +177,66 @@ var _ = Describe("[sriov] operator", func() {
 			err = namespaces.CleanNetworks(operatorNamespace, clients)
 			Expect(err).ToNot(HaveOccurred())
 		})
+		Context("Resource Injector", func() {
+			// 25815
+			It("Should inject downward api volume", func() {
+				sriovNetwork := &sriovv1.SriovNetwork{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "test-apivolnetwork",
+						Namespace: operatorNamespace,
+					},
+					Spec: sriovv1.SriovNetworkSpec{
+						ResourceName:     resourceName,
+						IPAM:             `{"type":"host-local","subnet":"10.10.10.0/24","rangeStart":"10.10.10.171","rangeEnd":"10.10.10.181","routes":[{"dst":"0.0.0.0/0"}],"gateway":"10.10.10.1"}`,
+						NetworkNamespace: namespaces.Test,
+					}}
+				err := clients.Create(context.Background(), sriovNetwork)
+				Expect(err).ToNot(HaveOccurred())
+
+				Eventually(func() error {
+					netAttDef := &netattdefv1.NetworkAttachmentDefinition{}
+					return clients.Get(context.Background(), runtimeclient.ObjectKey{Name: "test-apivolnetwork", Namespace: namespaces.Test}, netAttDef)
+				}, 10*time.Second, 1*time.Second).ShouldNot(HaveOccurred())
+
+				podDefinition := pod.DefineWithNetworks([]string{sriovNetwork.Name})
+				created, err := clients.Pods(namespaces.Test).Create(context.Background(), podDefinition, metav1.CreateOptions{})
+				Expect(err).ToNot(HaveOccurred())
+
+				var runningPod *corev1.Pod
+				Eventually(func() corev1.PodPhase {
+					runningPod, err = clients.Pods(namespaces.Test).Get(context.Background(), created.Name, metav1.GetOptions{})
+					Expect(err).ToNot(HaveOccurred())
+					return runningPod.Status.Phase
+				}, 3*time.Minute, time.Second).Should(Equal(corev1.PodRunning))
+
+				var downwardVolume *corev1.Volume
+				for _, v := range runningPod.Spec.Volumes {
+					if v.Name == "podnetinfo" {
+						downwardVolume = v.DeepCopy()
+						break
+					}
+				}
+
+				Expect(downwardVolume).ToNot(BeNil(), "Downward volume not found")
+				Expect(downwardVolume.DownwardAPI).ToNot(BeNil(), "Downward api not found in volume")
+				Expect(downwardVolume.DownwardAPI.Items).To(SatisfyAll(
+					ContainElement(corev1.DownwardAPIVolumeFile{
+						Path: "labels",
+						FieldRef: &corev1.ObjectFieldSelector{
+							APIVersion: "v1",
+							FieldPath:  "metadata.labels",
+						},
+					}), ContainElement(corev1.DownwardAPIVolumeFile{
+						Path: "annotations",
+						FieldRef: &corev1.ObjectFieldSelector{
+							APIVersion: "v1",
+							FieldPath:  "metadata.annotations",
+						},
+					})))
+			})
+
+		})
+
 		Context("VF flags", func() {
 			hostNetPod := &corev1.Pod{} // Initialized in BeforeEach
 			intf := &sriovv1.InterfaceExt{}
@@ -993,102 +1053,6 @@ var _ = Describe("[sriov] operator", func() {
 						pingPod(ip, testNode, sriovNetworkName)
 					}
 				})
-			})
-			Context("Resource Injector", func() {
-				// 25815
-				It("Should inject downward api volume", func() {
-					node := sriovInfos.Nodes[0]
-					intf, err := sriovInfos.FindOneSriovDevice(node)
-					Expect(err).ToNot(HaveOccurred())
-
-					nodePolicy := &sriovv1.SriovNetworkNodePolicy{
-						ObjectMeta: metav1.ObjectMeta{
-							GenerateName: "test-apivolumepolicy",
-							Namespace:    operatorNamespace,
-						},
-
-						Spec: sriovv1.SriovNetworkNodePolicySpec{
-							NodeSelector: map[string]string{
-								"kubernetes.io/hostname": node,
-							},
-							NumVfs:       5,
-							ResourceName: "apivolresource",
-							Priority:     99,
-							NicSelector: sriovv1.SriovNetworkNicSelector{
-								PfNames: []string{intf.Name},
-							},
-							DeviceType: "netdevice",
-						},
-					}
-
-					err = clients.Create(context.Background(), nodePolicy)
-					Expect(err).ToNot(HaveOccurred())
-
-					waitForSRIOVStable()
-
-					Eventually(func() int64 {
-						testedNode, err := clients.Nodes().Get(context.Background(), node, metav1.GetOptions{})
-						Expect(err).ToNot(HaveOccurred())
-						resNum, _ := testedNode.Status.Allocatable["openshift.io/apivolresource"]
-						capacity, _ := resNum.AsInt64()
-						return capacity
-					}, 3*time.Minute, time.Second).Should(Equal(int64(5)))
-
-					sriovNetwork := &sriovv1.SriovNetwork{
-						ObjectMeta: metav1.ObjectMeta{
-							Name:      "test-apivolnetwork",
-							Namespace: operatorNamespace,
-						},
-						Spec: sriovv1.SriovNetworkSpec{
-							ResourceName:     "apivolresource",
-							IPAM:             `{"type":"host-local","subnet":"10.10.10.0/24","rangeStart":"10.10.10.171","rangeEnd":"10.10.10.181","routes":[{"dst":"0.0.0.0/0"}],"gateway":"10.10.10.1"}`,
-							NetworkNamespace: namespaces.Test,
-						}}
-					err = clients.Create(context.Background(), sriovNetwork)
-					Expect(err).ToNot(HaveOccurred())
-
-					Eventually(func() error {
-						netAttDef := &netattdefv1.NetworkAttachmentDefinition{}
-						return clients.Get(context.Background(), runtimeclient.ObjectKey{Name: "test-apivolnetwork", Namespace: namespaces.Test}, netAttDef)
-					}, 10*time.Second, 1*time.Second).ShouldNot(HaveOccurred())
-
-					podDefinition := pod.DefineWithNetworks([]string{sriovNetwork.Name})
-					created, err := clients.Pods(namespaces.Test).Create(context.Background(), podDefinition, metav1.CreateOptions{})
-					Expect(err).ToNot(HaveOccurred())
-
-					var runningPod *corev1.Pod
-					Eventually(func() corev1.PodPhase {
-						runningPod, err = clients.Pods(namespaces.Test).Get(context.Background(), created.Name, metav1.GetOptions{})
-						Expect(err).ToNot(HaveOccurred())
-						return runningPod.Status.Phase
-					}, 3*time.Minute, time.Second).Should(Equal(corev1.PodRunning))
-
-					var downwardVolume *corev1.Volume
-					for _, v := range runningPod.Spec.Volumes {
-						if v.Name == "podnetinfo" {
-							downwardVolume = v.DeepCopy()
-							break
-						}
-					}
-
-					Expect(downwardVolume).ToNot(BeNil(), "Downward volume not found")
-					Expect(downwardVolume.DownwardAPI).ToNot(BeNil(), "Downward api not found in volume")
-					Expect(downwardVolume.DownwardAPI.Items).To(SatisfyAll(
-						ContainElement(corev1.DownwardAPIVolumeFile{
-							Path: "labels",
-							FieldRef: &corev1.ObjectFieldSelector{
-								APIVersion: "v1",
-								FieldPath:  "metadata.labels",
-							},
-						}), ContainElement(corev1.DownwardAPIVolumeFile{
-							Path: "annotations",
-							FieldRef: &corev1.ObjectFieldSelector{
-								APIVersion: "v1",
-								FieldPath:  "metadata.annotations",
-							},
-						})))
-				})
-
 			})
 
 			Context("MTU", func() {
