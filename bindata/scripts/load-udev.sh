617d1bf791c5890b9c0aa088b27401499a8a3737
Mamduh Alassi <mamduhala@mellanox.com>
2021-02-11 11:58:08 +0200

    Fix bindata/scripts/load-udev.sh to run on ubuntu RHEL/centos has udevadm in a different location than ubuntu which fails the script
    Signed-off-by: Mamduh Alassi <mamduhala@mellanox.com>

--- bindata/scripts/load-udev.sh
+++ bindata/scripts/load-udev.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
-echo "Reload udev rules: /usr/sbin/udevadm control --reload-rules"
-chroot /host/ /usr/sbin/udevadm control --reload-rules
+REDHAT_RELEASE_FILE="/host/etc/redhat-release"
+udevadm_bin=""
 
-echo "Trigger udev event: /usr/sbin/udevadm trigger --action add --attr-match subsystem=net"
-chroot /host/ /usr/sbin/udevadm trigger --action add --attr-match subsystem=net
+if [ -f "$REDHAT_RELEASE_FILE" ]; then
+  udevadm_bin="/usr/sbin/udevadm"
+elif grep -i --quiet 'ubuntu' /host/etc/os-release; then
+  if grep -i --quiet '20' /host/etc/os-release; then
+    udevadm_bin="/usr/bin/udevadm"
+  elif grep -i --quiet '16\|18\|14' /host/etc/os-release; then
+    udevadm_bin="/sbin/udevadm"
+  fi
+fi
+
+if [ -z "$udevadm_bin" ]; then
+  echo "udevadm not found"
+  exit 1
+fi
+
+echo "Reload udev rules: $udevadm_bin control --reload-rules"
+chroot /host/ $udevadm_bin control --reload-rules
+
+echo "Trigger udev event: $udevadm_bin trigger --action add --attr-match subsystem=net"
+chroot /host/ $udevadm_bin trigger --action add --attr-match subsystem=net
