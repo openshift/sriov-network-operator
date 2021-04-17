212febf2f21a363a20d7f63651d8f5a8c017d7fa
Kennelly, Martin <martin.kennelly@intel.com>
2021-04-17 16:06:57 +0100

    Move PF/VF netns monitoring and add logging
    Move PF/VF netns monitoring to test script to prevent
    escape processes during testing.
    
    Add logging to PF/VF monitoring. Increase in test log level
    required to view its output.
    
    PF/VF monitor does not exist when PF not found, assumes it is
    in correct netns and continues.
    
    Rename execution of KinD cluster and testing file to be
    consistent with existing files.
    
    Move e2e doc to docs.
    
    Signed-off-by: Kennelly, Martin <martin.kennelly@intel.com>

---
+++ doc/testing-kind.md
@@ -0,0 +1,25 @@
+## E2E test with KinD
+### How to test
+To execute E2E tests, a SR-IOV Physical Function device is required and will be added to a KinD workers network namespace.
+```
+$ git clone https://github.com/k8snetworkplumbingwg/sriov-network-operator.git
+$ cd sriov-network-operator/
+$ ./hack/get-e2e-kind-tools.sh
+$ export TEST_PCI_DEVICE=0000:02:00.0
+$ sudo ./hack/run-e2e-test-kind.sh $TEST_PCI_DEVICE
+```
+Note: Test device will remain in KinD worker node until cluster is terminated.
+
+### How to repeat test using existing KinD cluster
+Export test PCI device used to set up KinD cluster and export KinD worker network namespace path:
+```
+$ export TEST_PCI_DEVICE=0000:02:00.0
+$ export TEST_NETNS_PATH=$(docker inspect "$(docker ps --filter 'name=kind-worker' -q)" --format "{{ .NetworkSettings.SandboxKey }}")
+$ sudo make test-e2e-k8s
+```
+
+### How to teardown
+
+```
+$ ./hack/teardown-e2e-kind-cluster.sh
+```
--- test/util/netns/netns.go
+++ test/util/netns/netns.go
@@ -5,8 +5,10 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"runtime"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/vishvananda/netlink"
 	"github.com/vishvananda/netns"
@@ -17,9 +19,71 @@ const (
 	sriovNumVfsFile = "sriov_numvfs"
 )
 
-//SetVfNetNs requires physical function (PF) PCI address and a handle to a network namespace in which to add
+//SetPfVfLinkNetNs requires physical function (PF) PCI address and a string to a network namespace in which to add
+//any associated virtual functions (VF). VFs must be attached to kernel driver to provide links. Attaching VFs to
+//vfio-pci driver is not supported. PF is set to target network namespace.
+//Polling interval is required and this period will determine how often the VFs Links are checked to ensure they are in
+//the target network namespace. Two channels are required - one for informing the func to end and one to inform
+//the caller of an error or if the function has ended. It is this func responsibility to cleanup the done channel and
+//callers responsibility to cleanup quit channel.
+func SetPfVfLinkNetNs(pfPciAddr, netNsPath string, pollInterval time.Duration, quitCh chan bool, doneCh chan error) {
+	runtime.LockOSThread()
+	defer runtime.UnlockOSThread()
+	var errL []error
+	//merge any errors found during execution, send through channel, and close it.
+	defer func() {
+		if len(errL) != 0 {
+			errMsg := ""
+			for i, err := range errL {
+				errMsg = errMsg + fmt.Sprintf("error %d) '%s'", i+1, err.Error())
+			}
+			doneCh <- fmt.Errorf("%s", errMsg)
+		}
+		close(doneCh)
+	}()
+
+	if pfPciAddr == "" || netNsPath == "" {
+		errL = append(errL, fmt.Errorf("SetPfVfLinkNetNs(): specify PF PCI address and/or netns path '%s' & '%s'",
+			pfPciAddr, netNsPath))
+		return
+	}
+
+	pfPciDir := filepath.Join(sysBusPci, pfPciAddr)
+	if _, err := os.Lstat(pfPciDir); err != nil {
+		errL = append(errL, fmt.Errorf("SetPfVfLinkNetNs(): failed to find PCI device at '%s': '%s'", pfPciDir,
+			err.Error()))
+		return
+	}
+
+	targetNetNs, err := netns.GetFromPath(netNsPath)
+	if err != nil {
+		errL = append(errL, fmt.Errorf("SetPfVfLinkNetNs(): failed to get target network namespace: '%s'",
+			err.Error()))
+		return
+	}
+	defer targetNetNs.Close()
+	// if failure to set PF netns, emit error, assume its in correct netns and continue
+	if err = setLinkNetNs(pfPciAddr, targetNetNs); err != nil {
+		errL = append(errL, fmt.Errorf("SetPfVfLinkNetNs(): unable to set physical function '%s' network namespace: '%s'", pfPciAddr,
+			err.Error()))
+	}
+
+	for {
+		select {
+		case <-quitCh:
+			return
+		case <-time.Tick(pollInterval):
+			if err := setVfNetNs(pfPciAddr, targetNetNs); err != nil {
+				//save errors for returning but continue
+				errL = append(errL, err)
+			}
+		}
+	}
+}
+
+//setVfNetNs requires physical function (PF) PCI address and a handle to a network namespace in which to add
 //any associated virtual functions (VF). If no VFs are found, no error is returned.
-func SetVfNetNs(pfPciAddr string, targetNetNs netns.NsHandle) error {
+func setVfNetNs(pfPciAddr string, targetNetNs netns.NsHandle) error {
 	var (
 		err      error
 		link     netlink.Link
@@ -27,23 +91,23 @@ func SetVfNetNs(pfPciAddr string, targetNetNs netns.NsHandle) error {
 	)
 	numVfsFile := filepath.Join(sysBusPci, pfPciAddr, sriovNumVfsFile)
 	if _, err := os.Lstat(numVfsFile); err != nil {
-		return fmt.Errorf("unable to open '%s' from device with PCI address '%s': '%s", numVfsFile, pfPciAddr,
+		return fmt.Errorf("setVfNetNs(): unable to open '%s' from device with PCI address '%s': '%s", numVfsFile, pfPciAddr,
 			err.Error())
 	}
 
 	data, err := ioutil.ReadFile(numVfsFile)
 	if err != nil {
-		return fmt.Errorf("failed to read '%s' from device with PCI address  '%s': '%s", numVfsFile, pfPciAddr,
+		return fmt.Errorf("setVfNetNs(): failed to read '%s' from device with PCI address  '%s': '%s", numVfsFile, pfPciAddr,
 			err.Error())
 	}
 
 	if len(data) == 0 {
-		return fmt.Errorf("no data in file '%s'", numVfsFile)
+		return fmt.Errorf("setVfNetNs(): no data in file '%s'", numVfsFile)
 	}
 
 	vfTotal, err := strconv.Atoi(strings.TrimSpace(string(data)))
 	if err != nil {
-		return fmt.Errorf("unable to convert file '%s' to integer: '%s'", numVfsFile, err.Error())
+		return fmt.Errorf("setVfNetNs(): unable to convert file '%s' to integer: '%s'", numVfsFile, err.Error())
 	}
 
 	for vfNo := 0; vfNo <= vfTotal; vfNo++ {
@@ -55,7 +119,7 @@ func SetVfNetNs(pfPciAddr string, targetNetNs netns.NsHandle) error {
 
 		fInfos, err := ioutil.ReadDir(vfNetDir)
 		if err != nil {
-			return fmt.Errorf("failed to read '%s': '%s'", vfNetDir, err.Error())
+			return fmt.Errorf("setVfNetNs(): failed to read '%s': '%s'", vfNetDir, err.Error())
 		}
 		if len(fInfos) == 0 {
 			continue
@@ -64,40 +128,40 @@ func SetVfNetNs(pfPciAddr string, targetNetNs netns.NsHandle) error {
 		for _, f := range fInfos {
 			link, err = netlink.LinkByName(f.Name())
 			if err != nil {
-				return fmt.Errorf("unable to create link with name '%s' from directory '%s': '%s'", f.Name(),
+				return fmt.Errorf("setVfNetNs(): unable to get link with name '%s' from directory '%s': '%s'", f.Name(),
 					vfNetDir, err.Error())
 			}
 			if err = netlink.LinkSetNsFd(link, int(targetNetNs)); err != nil {
-				return fmt.Errorf("failed to set link '%s' to netns: '%s'", f.Name(), err.Error())
+				return fmt.Errorf("setVfNetNs(): failed to set link '%s' netns: '%s'", f.Name(), err.Error())
 			}
 		}
 	}
 	return nil
 }
 
-//SetLinkNetNs attempts to create a link object from PCI address and change the network namespace. Arg pciAddr must have
+//setLinkNetNs attempts to create a link object from PCI address and change the network namespace. Arg pciAddr must have
 //an associated interface name in the current network namespace or an error is thrown.
-func SetLinkNetNs(pciAddr string, targetNetNs netns.NsHandle) error {
+func setLinkNetNs(pciAddr string, targetNetNs netns.NsHandle) error {
 	var err error
 	netDir := filepath.Join(sysBusPci, pciAddr, "net")
 	if _, err := os.Lstat(netDir); err != nil {
-		return fmt.Errorf("unable to find directory '%s': '%s'", netDir, err.Error())
+		return fmt.Errorf("setLinkNetNs(): unable to find directory '%s': '%s'", netDir, err.Error())
 	}
 	fInfos, err := ioutil.ReadDir(netDir)
 	if err != nil {
-		return fmt.Errorf("failed to read '%s': '%s'", netDir, err.Error())
+		return fmt.Errorf("setLinkNetNs(): failed to read '%s': '%s'", netDir, err.Error())
 	}
 	if len(fInfos) == 0 {
-		return fmt.Errorf("no files found at directory '%s'", netDir)
+		return fmt.Errorf("setLinkNetNs(): no files found at directory '%s'", netDir)
 	}
 	link, err := netlink.LinkByName(fInfos[0].Name())
 	if err != nil {
-		return fmt.Errorf("failed to create link object by name '%s' from files at '%s': '%s'", fInfos[0].Name(),
+		return fmt.Errorf("setLinkNetNs(): failed to create link object by name '%s' from files at '%s': '%s'", fInfos[0].Name(),
 			netDir, err.Error())
 	}
 	err = netlink.LinkSetNsFd(link, int(targetNetNs))
 	if err != nil {
-		return fmt.Errorf("failed to set link '%s' netns: '%s'", link.Attrs().Name, err.Error())
+		return fmt.Errorf("setLinkNetNs(): failed to set link '%s' netns: '%s'", link.Attrs().Name, err.Error())
 	}
 	return nil
 }
