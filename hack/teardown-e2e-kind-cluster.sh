3810f55d63d5d59074c0f83ddd5232f9f51c10fe
Martin Kennelly <martin.kennelly@intel.com>
2021-06-10 14:31:09 +0100

    Fix adding relative path to PATH
    Signed-off-by: Martin Kennelly <martin.kennelly@intel.com>

--- hack/teardown-e2e-kind-cluster.sh
+++ hack/teardown-e2e-kind-cluster.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 # Teardown KinD cluster
 
-root="$(dirname "$0")/../"
-export PATH="${PATH}:${root:?}bin"
-
 if ! command -v kind &> /dev/null; then
   echo "KinD is not available"
   exit 1
