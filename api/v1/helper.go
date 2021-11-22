dc1a4fa6a6245e7c6ddb9e761beb8de1bd9ed7c3
Peng Liu <pliu@redhat.com>
2021-11-22 03:15:00 -0500

    Reload VF driver only when rdma is enabled
    To set the GUID to VF, we need to set the admin MAC and reload the
    VF driver. It will take a long time if users set big 'numVFs'. This
    PR changes the logic to only reload the VF driver when isRdma is
    set. So that, users not using ROCE will no longer need to wait for
    the VF driver reload.

--- api/v1/helper.go
+++ api/v1/helper.go
@@ -348,6 +348,7 @@ func (p *SriovNetworkNodePolicy) generateVfGroup(iface *InterfaceExt) (*VfGroup,
 		VfRange:      rng,
 		PolicyName:   p.GetName(),
 		Mtu:          p.Spec.Mtu,
+		IsRdma:       p.Spec.IsRdma,
 	}, nil
 }
 
