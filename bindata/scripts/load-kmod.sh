16df26a49117b467fd608265acb04b7513a3430a
Emilien Macchi <emilien@redhat.com>
2022-02-23 15:32:22 -0500

    virtual/openstack: configure VFIO with NIOMMU by default
    As well known and well documented, in virtual deployments of Kubernetes such as running on top
    of OpenStack where the underlying virtualization platform is KVM, it does not support a virtualized iommu,
    the VFIO PCI driver needs to be loaded with a special flag.
    
    This patch aims to do it by default in the virtual_plugin, since this
    plugin is only used by OpenStack for now.
    
    Note: it also changes `utils.LoadKernelModule` function to support
    kernel module arguments. It also adds a note in `load-kmod.sh` about the
    fact that a module won't be reloaded with potential new options if the
    module is already loaded on the system.

--- bindata/scripts/load-kmod.sh
+++ bindata/scripts/load-kmod.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 # chroot /host/ modprobe $1
 kmod_name=$(tr "-" "_" <<< $1)
+kmod_args="${@:2}"
 chroot /host/ lsmod | grep "^$1" >& /dev/null
 
 if [ $? -eq 0 ]
 then
+        # NOTE: We do not check if the module is loaded with specific options
+        #       so a manual reload is required if the module is loaded with
+        #       new or different options.
+        echo "Module $kmod_name already loaded; no change will be applied..."
         exit 0 
 else
-        chroot /host/ modprobe $kmod_name
+        chroot /host/ modprobe $kmod_name $kmod_args
 fi
