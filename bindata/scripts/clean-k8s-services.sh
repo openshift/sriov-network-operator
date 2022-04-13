160531b922b35faf64e4fb15e12fac9653b07da3
Fred Rolland <frolland@nvidia.com>
2022-04-13 08:41:18 +0300

    Remove switchdev scripts when uneeded
    Fixes issue #249
    
    Signed-off-by: Fred Rolland <frolland@nvidia.com>

--- bindata/scripts/clean-k8s-services.sh
+++ bindata/scripts/clean-k8s-services.sh
@@ -9,10 +9,16 @@ chroot_path="/host"
 
 function clean_services() {
   # Remove switchdev service files
-  rm -f $chroot_path/etc/systemd/system/switchdev-configuration.service
-  rm -f $chroot_path/usr/local/bin/configure-switchdev.sh
+  rm -f $chroot_path/etc/systemd/system/switchdev-configuration-after-nm.service
+  rm -f $chroot_path/etc/systemd/system/switchdev-configuration-before-nm.service
+  rm -f $chroot_path/usr/local/bin/switchdev-configuration-after-nm.sh
+  rm -f $chroot_path/usr/local/bin/switchdev-configuration-before-nm.sh
   rm -f $chroot_path/etc/switchdev.conf
   rm -f $chroot_path/etc/udev/switchdev-vf-link-name.sh
+  # The following files are no longer created by config daemon
+  # Remove them in case of leftovers from earlier SR-IOV network operator
+  rm -f $chroot_path/usr/local/bin/configure-switchdev.sh
+  rm -f $chroot_path/etc/systemd/system/switchdev-configuration.service
 
   # clean NetworkManager and ovs-vswitchd services
   network_manager_service=$chroot_path/usr/lib/systemd/system/NetworkManager.service
