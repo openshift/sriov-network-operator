e6926a0af2e3237a91858cc5a400c4600d731d7b
Andrea Panattoni <apanatto@redhat.com>
2022-05-05 13:08:52 +0200

    Improve `GetSriovNicIPs` error message
    In case of error, it is useful to know the reason why it
    was not possible to get the IP address of SR-IOV interface.
    
    Add description to each returned error and return an error
    in case the requested interface is not found.
    
    Add "github.com/stretchr/testify/assert" dependency adn
    unit tests for GetSriovNicIPs
    
    Signed-off-by: Andrea Panattoni <apanatto@redhat.com>

--- test/util/network/network.go
+++ test/util/network/network.go
@@ -98,10 +98,16 @@ func GetNicsByPrefix(pod *k8sv1.Pod, ifcPrefix string) ([]string, error) {
 // GetSriovNicIPs returns the list of ip addresses related to the given
 // interface name for the given pod.
 func GetSriovNicIPs(pod *k8sv1.Pod, ifcName string) ([]string, error) {
+
+	networksStatus, ok := pod.ObjectMeta.Annotations["k8s.v1.cni.cncf.io/networks-status"]
+	if !ok {
+		return nil, fmt.Errorf("pod [%s] has no annotation `k8s.v1.cni.cncf.io/networks-status`", pod.Name)
+	}
+
 	var nets []Network
-	err := json.Unmarshal([]byte(pod.ObjectMeta.Annotations["k8s.v1.cni.cncf.io/networks-status"]), &nets)
+	err := json.Unmarshal([]byte(networksStatus), &nets)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("can't unmarshal annotation `k8s.v1.cni.cncf.io/networks-status`: %w", err)
 	}
 	for _, net := range nets {
 		if net.Interface != ifcName {
@@ -109,7 +115,8 @@ func GetSriovNicIPs(pod *k8sv1.Pod, ifcName string) ([]string, error) {
 		}
 		return net.Ips, nil
 	}
-	return nil, nil
+
+	return nil, fmt.Errorf("interface [%s] not found in pod annotation", ifcName)
 }
 
 // Return a definition of a macvlan NetworkAttachmentDefinition
