8c972720e2cee976e66b4c517be3cb245ff3e169
adrianc <adrianc@nvidia.com>
2024-02-05 19:41:49 +0200

    Change sriov device plugin owner ref
    Use default SriovOperatorConfig object instead
    of default SriovNetworkNodePolicy.
    
    This change is required as subsequent changes
    will drop the creation of the default policy.
    
    Signed-off-by: adrianc <adrianc@nvidia.com>

--- controllers/sriovoperatorconfig_controller.go
+++ controllers/sriovoperatorconfig_controller.go
@@ -140,7 +140,7 @@ func (r *SriovOperatorConfigReconciler) Reconcile(ctx context.Context, req ctrl.
 		return reconcile.Result{}, err
 	}
 
-	if err = syncPluginDaemonObjs(ctx, r.Client, r.Scheme, defaultPolicy, policyList); err != nil {
+	if err = syncPluginDaemonObjs(ctx, r.Client, r.Scheme, defaultConfig, policyList); err != nil {
 		return reconcile.Result{}, err
 	}
 
