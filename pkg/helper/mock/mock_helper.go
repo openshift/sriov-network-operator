d1775939af312643e27274a20a58713845cca3f9
Tobias Giese <tgiese@nvidia.com>
2024-07-11 13:39:25 +0200

    feat: implement MlxResetFW to reset the FW on VF changes
    Signed-off-by: Tobias Giese <tgiese@nvidia.com>

--- pkg/helper/mock/mock_helper.go
+++ pkg/helper/mock/mock_helper.go
@@ -722,6 +722,20 @@ func (mr *MockHostHelpersInterfaceMockRecorder) MlxConfigFW(attributesToChange i
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "MlxConfigFW", reflect.TypeOf((*MockHostHelpersInterface)(nil).MlxConfigFW), attributesToChange)
 }
 
+// MlxResetFW mocks base method.
+func (m *MockHostHelpersInterface) MlxResetFW(pciAddresses []string) error {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "MlxResetFW", pciAddresses)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// MlxResetFW indicates an expected call of MlxResetFW.
+func (mr *MockHostHelpersInterfaceMockRecorder) MlxResetFW(pciAddresses interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "MlxResetFW", reflect.TypeOf((*MockHostHelpersInterface)(nil).MlxResetFW), pciAddresses)
+}
+
 // MstConfigReadData mocks base method.
 func (m *MockHostHelpersInterface) MstConfigReadData(arg0 string) (string, string, error) {
 	m.ctrl.T.Helper()
