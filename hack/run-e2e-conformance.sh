466788d9f19498ab0ee5c2d271991704f8c66595
Sagi Shnaidman <sshnaidm@redhat.com>
2024-08-07 21:26:23 +0300

    Donot merge: WIP testing patch

--- hack/run-e2e-conformance.sh
+++ hack/run-e2e-conformance.sh
@@ -8,3 +8,5 @@ JUNIT_OUTPUT="${JUNIT_OUTPUT:-/tmp/artifacts}"
 export PATH=$PATH:$GOPATH/bin
 
 ${root}/bin/ginkgo -output-dir=$JUNIT_OUTPUT --junit-report "unit_report.xml" -v "$SUITE" -- -report=$JUNIT_OUTPUT
+
+# test my patch
