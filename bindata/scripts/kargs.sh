81c67cce3f56d53fb935835bbeab042ac4ebac6f
Ivan Kolodiazhnyi <ikolodiazhny@nvidia.com>
2024-12-17 14:19:38 +0200

    Skip kernel parameters configuration for Ubuntu
    It's enouph to configure ib_core module in /etc/moprobe.d/ for
    Ubuntu OS to change RDMA subsystem mode.
    
    Also this commit add OS check into  kargs.sh error because 'grubby'
    isn't available in official Ubuntu repositories.
    
    Kernel param configuration support in Ubuntu should be implemented
    in a separate commit.
    
    Signed-off-by: Ivan Kolodiazhnyi <ikolodiazhny@nvidia.com>

--- bindata/scripts/kargs.sh
+++ bindata/scripts/kargs.sh
@@ -7,6 +7,14 @@ declare -a kargs=( "$@" )
 ret=0
 args=$(chroot /host/ cat /proc/cmdline)
 
+IS_OS_UBUNTU=true; [[ "$(chroot /host/ grep -i ubuntu /etc/os-release -c)" == "0" ]] && IS_OS_UBUNTU=false
+
+# Kernel args configuration isn't supported for Ubuntu now, so we shouldn't do anything here
+if ${IS_OS_UBUNTU} ; then
+    echo $ret
+    exit 0
+fi
+
 if chroot /host/ test -f /run/ostree-booted ; then
     for t in "${kargs[@]}";do
         if [[ $command == "add" ]];then
