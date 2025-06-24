5656b642de8c4adce3adf684ed566d0ae7e8536f
Andrea Panattoni <apanatto@redhat.com>
2025-06-24 12:30:33 +0200

    ds: bump operator to 4.20
    Signed-off-by: Andrea Panattoni <apanatto@redhat.com>

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -10,10 +10,10 @@ import (
 var (
 	// Raw is the string representation of the version. This will be replaced
 	// with the calculated version at build time.
-	Raw = "v4.19.0"
+	Raw = "v4.20.0"
 
 	// Version is semver representation of the version.
-	Version = semver.MustParse("4.19.0")
+	Version = semver.MustParse("4.20.0")
 
 	// String is the human-friendly representation of the version.
 	String = fmt.Sprintf("SriovNetworkConfigOperator %s", Raw)
