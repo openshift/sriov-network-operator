1a1b17e5091a43ea00b2359eab9f76be825f4a3b
Sebastian Sch <sebassch@gmail.com>
2025-07-30 16:50:54 +0300

    fix apply after PR#913
    The following PR remove the kustomize
    
    https://github.com/k8snetworkplumbingwg/sriov-network-operator/pull/913
    
    Signed-off-by: Sebastian Sch <sebassch@gmail.com>

--- hack/run-e2e-conformance-virtual-ocp.sh
+++ hack/run-e2e-conformance-virtual-ocp.sh
@@ -277,7 +277,7 @@ podman rmi -fi ${SRIOV_NETWORK_WEBHOOK_IMAGE}
 podman logout $registry
 
 echo "## apply CRDs"
-kubectl apply -k $root/config/crd
+kubectl apply -f $root/config/crd/bases
 
 
 cat <<EOF | kubectl apply -f -
